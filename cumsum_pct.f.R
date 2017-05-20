0624b323f6311880bb6cabee7471952bee32d220
chickentheory <crispychickentheory@gmail.com>
2017-05-20 13:48:43 -0500

    Create cumsum_pct.f.R

---
+++ cumsum_pct.f.R
@@ -0,0 +1,17 @@
+##################################################
+# AUTHOR: ChickenTheory
+# PURPOSE:
+#   Percentage of running total.
+#   Based on R's cumsum().
+# INPUT: A vector.
+# OUTPUT: A vector.
+# PITCH(?):
+#  EVER RAN A CUMULATIVE SUM AND
+#  WONDERED "GEE, I WISH I HAD
+#  THIS IN PERCENTAGE FORM!!!"?
+#  WELL, HERE YOU GO!!!!!!!!!!!!
+##################################################
+
+cumsum_pct.f <- function(x) {cumsum(x)/sum(x)}
+
+### AMAZING!!! ###
