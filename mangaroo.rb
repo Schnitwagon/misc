49c96706c1a62e9eec016585a1cd26e4b2aa40c9
chickentheory <crispychickentheory@gmail.com>
2017-05-20 13:59:09 -0500

    Create mangaroo.rb

---
+++ mangaroo.rb
@@ -0,0 +1,24 @@
+################################################
+# TITLE: Top 10 Things I Like about Reborn
+# AUTHOR: ChickenTheory
+# IDEA: for each i in a set of elements, 
+#       return "[integer]. Mangaroo"
+# PURPOSE: ?????
+#          Well, I mean, 
+#          he's the best character...
+################################################
+
+##### === BEGIN === #####
+
+# THE ARRAY FOR NUMBERED LIST
+  x = Array(1..10)
+
+# TITLE FOR LIST
+  puts "Top 10 Things I like About Reborn"
+
+# THE LOOP
+  x.each do|n|
+	  puts "#{n}. Mangaroo"
+  end
+  
+##### === END === #####
