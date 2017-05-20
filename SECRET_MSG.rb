20bf609bc7b97289cdb0f79e7f1990a83f805da7
chickentheory <crispychickentheory@gmail.com>
2017-05-20 14:09:15 -0500

    Create SECRET_MSG.rb

---
+++ SECRET_MSG.rb
@@ -0,0 +1,24 @@
+#################################
+# AUTHOR: ChickenTheory
+# DESCRIPTION:
+#  A SECRET MESSAGE!! FOR YOU!!
+#################################
+
+##### === BEGIN === #####
+
+  #### THE SET UP!!! ####
+    y = [1,2,3]
+    x = "ecafttub a r u"
+
+  #### THE FUNCTION!!! ####
+    def secret_msg(x,y)
+      puts "U RDY?"
+      puts y.reverse
+      puts x.reverse.upcase
+      10.times {puts "COOL"}
+    end
+    
+  #### THE MESSAGE!!! ####
+    secret_msg(x,y)   
+
+##### === END === #####
