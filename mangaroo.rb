cfd630b0242ff78a9cd7ceeff45121da778780c3
chickentheory <crispychickentheory@gmail.com>
2017-05-20 19:43:38 -0500

    Update mangaroo.rb
    best character

--- mangaroo.rb
+++ mangaroo.rb
@@ -1,8 +1,6 @@
 ################################################
 # TITLE: Top 10 Things I Like about Reborn
 # AUTHOR: ChickenTheory
-# IDEA: for each i in a set of elements, 
-#       return "[integer]. Mangaroo"
 # PURPOSE: ?????
 #          Well, I mean, 
 #          he's the best character...
@@ -14,7 +12,7 @@
   x = Array(1..10)
 
 # TITLE FOR LIST
-  puts "Top 10 Things I like About Reborn"
+  puts "Top 10 Things I Like About Reborn"
 
 # THE LOOP
   x.each do|n|
