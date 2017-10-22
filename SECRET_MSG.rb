be1dd72afa871b33362688193388129010144869
Chicken Theory <crispychickentheory@gmail.com>
2017-10-22 05:29:45 -0500

    onegai inputs
    Reordered input objects after the function definition. End of file now executes secret message.

--- SECRET_MSG.rb
+++ SECRET_MSG.rb
@@ -6,19 +6,19 @@
 
 ##### === BEGIN === #####
 
-  #### THE SET UP!!! ####
+  #### A VERY SECRET FUNCTION! ####
+    def secret_msg(a,b)
+       puts "U RDY?"
+       puts a.reverse
+       puts b.reverse.upcase
+       10.times {puts "COOL"}
+    end
+    
+  #### USE THESE INPUTS, PLEASE!!! ####
     y = [1,2,3]
     x = "ecafttub a r u"
 
-  #### THE FUNCTION!!! ####
-    def secret_msg(x,y)
-      puts "U RDY?"
-      puts y.reverse
-      puts x.reverse.upcase
-      10.times {puts "COOL"}
-    end
-    
-  #### THE MESSAGE!!! ####
-    secret_msg(x,y)   
+  #### EXECUTE! FUNCTION! ####
+  secret_msg(x,y)
 
 ##### === END === #####
